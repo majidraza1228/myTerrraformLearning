8acfe5cab0a10b7db793b1c2a2892a6baa1f516f
syedraza <majidraza@hotmail.com>
2023-09-03 22:40:12 -0400

    my first module commit

--- MySecondscript/main.tf
+++ MySecondscript/main.tf
@@ -269,18 +269,18 @@ resource "aws_security_group" "vpc-web" {
 }
 
 # Terraform Resource Block - To Build Web Server in Public Subnet
-resource "aws_instance" "web_server" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.generated.key_name
-  connection {
-    user        = "ubuntu"
-    private_key = tls_private_key.generated.private_key_pem
-    host        = self.public_ip
-  }
+# resource "aws_instance" "web_server" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = "t2.micro"
+#   subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
+#   security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+#   associate_public_ip_address = true
+#   key_name                    = aws_key_pair.generated.key_name
+#   connection {
+#     user        = "ubuntu"
+#     private_key = tls_private_key.generated.private_key_pem
+#     host        = self.public_ip
+#   }
 
   # Leave the first part of the block unchanged and create our `local-exec` provisioner
   provisioner "local-exec" {
